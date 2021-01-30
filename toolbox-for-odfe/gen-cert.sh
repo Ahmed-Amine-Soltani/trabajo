3cc23fa9b905686cee2dd4b8f0840692b2048f8a
ahmed amine soltani <soltani.ahmedamine1@gmail.com>
2021-01-30 20:48:58 +0100

    Toolbox for odfe generate node and client certificates script Sat 30 Jan 2021 08:48:58 PM CET

--- toolbox-for-odfe/gen-cert.sh
+++ toolbox-for-odfe/gen-cert.sh
@@ -4,10 +4,10 @@
 openssl genrsa -out root-ca-key.pem 2048
 openssl req -new -x509 -sha256 -key root-ca-key.pem -out root-ca.pem
 # Admin cert
-#openssl genrsa -out admin-key-temp.pem 2048
-#openssl pkcs8 -inform PEM -outform PEM -in admin-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out admin-key.pem
-#openssl req -new -key admin-key.pem -out admin.csr
-#openssl x509 -req -in admin.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out admin.pem
+openssl genrsa -out admin-key-temp.pem 2048
+openssl pkcs8 -inform PEM -outform PEM -in admin-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out admin-key.pem
+openssl req -new -key admin-key.pem -out admin.csr
+openssl x509 -req -in admin.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out admin.pem
 # Node cert
 openssl genrsa -out node-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in node-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out node-key.pem
