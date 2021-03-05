bc1e485186adc4f54280e41356fc4212430c367c
ahmed amine soltani <soltani.ahmedamine1@gmail.com>
2021-03-05 17:03:50 +0100

    spring boot web app for test

---
+++ spring_boot_web_app/src/main/java/com/example/app/AppMain.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+ 
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class AppMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class, args);
+        
+        
+    }
+}
+    
+    
---
+++ spring_boot_web_app/src/main/java/com/example/app/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+ 
+}
